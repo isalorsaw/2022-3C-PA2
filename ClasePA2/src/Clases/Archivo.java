cd912d83c2ee159554c03608a8bf02b05a79876f
isalorsaw <isalorsaw@hotmail.com>
2022-09-30 00:15:27 -0600

    Actualizacion de Archivos.java

--- ClasePA2/src/Clases/Archivo.java
+++ ClasePA2/src/Clases/Archivo.java
@@ -1,16 +1,34 @@
 package Clases;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.swing.JOptionPane;
 public class Archivo 
 {
 	String nomarchivo;
 	public static void main(String args[])
 	{
+		
 		Archivo a=new Archivo("src/Archivos/config.txt");
 		String info=a.traeString();
-		JOptionPane.showMessageDialog(null,info);
-		String l=a.traeLinea(1);
+		String l=a.traeLinea(2);
+		JOptionPane.showMessageDialog(null,l);
+		/*
+		try {
+			a.guardarEnPos("localhost;colinsoft;root;olimpia3;",2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+	}
+	public Archivo()
+	{
+		String n=DatosSesion.ruta;
+		JOptionPane.showMessageDialog(null,n);
+		new Archivo(n);
 	}
 	public Archivo(String na)
 	{
@@ -47,5 +65,37 @@ public class Archivo
 		String arr[]=traeString().split("\n");
 		if(i>0&&i<=arr.length)return arr[i-1];
 		else return null;
+	}
+	public void guardarString(String ultima)throws IOException
+	{
+		String lineas[]=traeString().split("\n");
+		PrintWriter g=new PrintWriter(nomarchivo);
+		g.flush();
+		for(int i=0;i<=lineas.length-1;i++)
+		{
+			g.write(lineas[i]);
+			g.println();
+		}
+		g.write(ultima);
+		g.println();
+		g.close();
+	}
+	public void guardarArr(String lineas[])throws IOException
+	{
+		PrintWriter g=new PrintWriter(nomarchivo);
+		g.flush();
+		for(int i=0;i<=lineas.length-1;i++)
+		{
+			g.write(lineas[i].trim());
+			g.println();
+		}
+		//g.println();
+		g.close();
+	}
+	public void guardarEnPos(String cadena, int p)throws IOException
+	{
+		String l[]=traeString().split("\n");
+		l[p-1]=cadena;
+		guardarArr(l);
 	}
 }
--- ClasePA2/src/frmConfigBD.java
+++ ClasePA2/src/frmConfigBD.java
@@ -5,12 +5,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Clases.Archivo;
+import Clases.Conexion;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class frmConfigBD extends JFrame {
 
@@ -19,7 +25,8 @@ public class frmConfigBD extends JFrame {
 	private JTextField txtbd;
 	private JTextField txtusuario;
 	private JPasswordField txtclave;
-
+	Conexion con=new Conexion();
+	Archivo arc=new Archivo("src/Archivos/config.txt");
 	/**
 	 * Launch the application.
 	 */
@@ -40,6 +47,7 @@ public class frmConfigBD extends JFrame {
 	 * Create the frame.
 	 */
 	public frmConfigBD() {
+		
 		setTitle("Configuracion Base de Datos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 382);
@@ -89,10 +97,66 @@ public class frmConfigBD extends JFrame {
 		contentPane.add(txtclave);
 		
 		JButton btnNewButton = new JButton("Conectar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				conectar();
+			}
+		});
 		btnNewButton.setBounds(169, 292, 89, 23);
 		contentPane.add(btnNewButton);
 		
-		cargarDatos();
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				cargarDatos();
+			}
+		});
+		//cargarDatos();
+	}
+	public void conectar()
+	{
+		boolean v=validar();
+		if(v)
+		{
+			Conexion con2=new Conexion(txtubicacion.getText(),txtbd.getText(),txtusuario.getText(),String.valueOf(txtclave.getPassword()));
+			if(!con2.getConexionState())
+			JOptionPane.showMessageDialog(null,"Error de Conexion","Error de Conexion",JOptionPane.ERROR_MESSAGE);
+			
+			else
+			{
+				int r=JOptionPane.showConfirmDialog(this,"Seguro Desea Modificar los Datos de la BD");
+				if(r==0)
+				{
+					String i=txtubicacion.getText()+";"+txtbd.getText()+";"+
+					txtusuario.getText()+";"+String.valueOf(txtclave.getPassword())+";";
+					try {
+						arc.guardarEnPos(i,2);
+						JOptionPane.showMessageDialog(null,"Guardado Satisfactoriamente");
+						dispose();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+	public boolean validar()
+	{
+		if(txtubicacion.getText().isEmpty())
+		JOptionPane.showMessageDialog(null,"Llenar Ubicacion","Invalido",JOptionPane.ERROR_MESSAGE);
+	
+		else if(txtbd.getText().isEmpty())
+		JOptionPane.showMessageDialog(null,"Ingrese Base de Datos","Invalido",JOptionPane.ERROR_MESSAGE);
+		
+		else if(txtusuario.getText().isEmpty())
+		JOptionPane.showMessageDialog(null,"Ingrese Usuario","Invalido",JOptionPane.ERROR_MESSAGE);
+		
+		else if(String.valueOf(txtclave.getPassword()).isEmpty())
+		JOptionPane.showMessageDialog(null,"Ingrese Clave","Invalido",JOptionPane.ERROR_MESSAGE);
+		
+		else return true;
+		return false;
 	}
 	public void cargarDatos()
 	{
@@ -104,6 +168,5 @@ public class frmConfigBD extends JFrame {
 		txtbd.setText(arr[1]);
 		txtusuario.setText(arr[2]);
 		txtclave.setText(arr[3]);
-		
 	}
 }
