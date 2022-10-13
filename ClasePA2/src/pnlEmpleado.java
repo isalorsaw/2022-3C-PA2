08e5bf8560c40360d876fc1124e5e8e6b381a827
isalorsaw <isalorsaw@hotmail.com>
2022-10-12 20:56:32 -0600

    Validar y accionar botones

--- ClasePA2/src/pnlEmpleado.java
+++ ClasePA2/src/pnlEmpleado.java
@@ -6,20 +6,22 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import com.toedter.calendar.JDateChooser;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class pnlEmpleado extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JComboBox textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JComboBox textField_5;
-	private JComboBox textField_6;
+	private JTextField txtidentidad;
+	private JTextField txtnombre, txtmovil, txtotro;
+	private JComboBox cmbdepto,cmbgenero;
+	private JTextField txtdireccion;
+	private JTextField txtcorreoe;
+	private JComboBox cmbestadoc;
+	private JComboBox cmbestado;
 	private JTextField txtdate;
 	private JTextField txtdate2;
 	private JDateChooser dfechan;
 	private JDateChooser dfechai;
-
+	private JButton btnGuardar;
 	/**
 	 * Create the panel.
 	 */
@@ -34,50 +36,74 @@ public class pnlEmpleado extends JPanel {
 		add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Nuevo");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiar();
+				btnGuardar.setText("GUARDAR");
+			}
+		});
 		btnNewButton.setBounds(46, 65, 89, 23);
 		add(btnNewButton);
 		
 		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiar();
+				btnGuardar.setText("MODIFICAR");
+			}
+		});
 		btnModificar.setBounds(174, 65, 89, 23);
 		add(btnModificar);
 		
 		JButton btnEliminar = new JButton("Deshabilitar");
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiar();
+				btnGuardar.setText("DAR DE BAJA");
+			}
+		});
 		btnEliminar.setBounds(313, 65, 89, 23);
 		add(btnEliminar);
 		
 		JButton btnRestaurar = new JButton("Habilitar");
+		btnRestaurar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiar();
+				btnGuardar.setText("RESTAURAR");
+			}
+		});
 		btnRestaurar.setBounds(448, 65, 89, 23);
 		add(btnRestaurar);
 		
-		JButton btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(262, 372, 89, 23);
+		btnGuardar = new JButton("Guardar");
+		btnGuardar.setBounds(262, 372, 140, 23);
 		add(btnGuardar);
 		
 		JLabel lblNewLabel_1 = new JLabel("Identidad");
 		lblNewLabel_1.setBounds(26, 111, 109, 14);
 		add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setBounds(145, 108, 274, 20);
-		add(textField);
-		textField.setColumns(10);
+		txtidentidad = new JTextField();
+		txtidentidad.setBounds(145, 108, 274, 20);
+		add(txtidentidad);
+		txtidentidad.setColumns(10);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Nombre ");
 		lblNewLabel_1_1.setBounds(26, 139, 109, 14);
 		add(lblNewLabel_1_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(145, 136, 274, 20);
-		add(textField_1);
+		txtnombre = new JTextField();
+		txtnombre.setColumns(10);
+		txtnombre.setBounds(145, 136, 274, 20);
+		add(txtnombre);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Departamento");
 		lblNewLabel_1_1_1.setBounds(26, 164, 109, 14);
 		add(lblNewLabel_1_1_1);
 		
-		textField_2 = new JComboBox();
-		textField_2.setBounds(145, 161, 274, 20);
-		add(textField_2);
+		cmbdepto = new JComboBox();
+		cmbdepto.setBounds(145, 161, 274, 20);
+		add(cmbdepto);
 		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("Fecha Ingreso");
 		lblNewLabel_1_1_1_1.setBounds(293, 189, 109, 14);
@@ -95,55 +121,55 @@ public class pnlEmpleado extends JPanel {
 		lblNewLabel_1_1_2.setBounds(26, 265, 109, 14);
 		add(lblNewLabel_1_1_2);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(145, 234, 274, 20);
-		add(textField_3);
+		txtdireccion = new JTextField();
+		txtdireccion.setColumns(10);
+		txtdireccion.setBounds(145, 234, 274, 20);
+		add(txtdireccion);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(145, 262, 274, 20);
-		add(textField_4);
+		txtcorreoe = new JTextField();
+		txtcorreoe.setColumns(10);
+		txtcorreoe.setBounds(145, 262, 274, 20);
+		add(txtcorreoe);
 		
 		JLabel lblNewLabel_1_1_1_1_1_1 = new JLabel("Genero");
 		lblNewLabel_1_1_1_1_1_1.setBounds(26, 212, 118, 14);
 		add(lblNewLabel_1_1_1_1_1_1);
 		
-		textField_5 = new JComboBox();
-		textField_5.setBounds(399, 209, 134, 20);
-		add(textField_5);
+		cmbestadoc = new JComboBox();
+		cmbestadoc.setBounds(399, 209, 134, 20);
+		add(cmbestadoc);
 		
 		JLabel lblNewLabel_1_1_1_1_2 = new JLabel("Estado Civil");
 		lblNewLabel_1_1_1_1_2.setBounds(293, 214, 109, 14);
 		add(lblNewLabel_1_1_1_1_2);
 		
-		JComboBox textField_5_1 = new JComboBox();
-		textField_5_1.setBounds(145, 209, 134, 20);
-		add(textField_5_1);
+		cmbgenero = new JComboBox();
+		cmbgenero.setBounds(145, 209, 134, 20);
+		add(cmbgenero);
 		
 		JLabel lblNewLabel_1_1_1_1_1_1_1 = new JLabel("Movil");
 		lblNewLabel_1_1_1_1_1_1_1.setBounds(26, 293, 118, 14);
 		add(lblNewLabel_1_1_1_1_1_1_1);
 		
-		JTextField textField_5_1_1 = new JTextField();
-		textField_5_1_1.setBounds(145, 290, 134, 20);
-		add(textField_5_1_1);
+		txtmovil = new JTextField();
+		txtmovil.setBounds(145, 290, 134, 20);
+		add(txtmovil);
 		
 		JLabel lblNewLabel_1_1_1_1_2_1 = new JLabel("Otro");
 		lblNewLabel_1_1_1_1_2_1.setBounds(293, 295, 109, 14);
 		add(lblNewLabel_1_1_1_1_2_1);
 		
-		JComboBox textField_5_2 = new JComboBox();
-		textField_5_2.setBounds(399, 293, 134, 20);
-		add(textField_5_2);
+		txtotro = new JTextField();
+		txtotro.setBounds(399, 293, 134, 20);
+		add(txtotro);
 		
 		JLabel lblNewLabel_1_1_1_1_1_1_1_1 = new JLabel("Estado");
 		lblNewLabel_1_1_1_1_1_1_1_1.setBounds(26, 321, 118, 14);
 		add(lblNewLabel_1_1_1_1_1_1_1_1);
 		
-		textField_6 = new JComboBox();
-		textField_6.setBounds(145, 318, 134, 20);
-		add(textField_6);
+		cmbestado = new JComboBox();
+		cmbestado.setBounds(145, 318, 134, 20);
+		add(cmbestado);
 		
 		dfechan = new JDateChooser();
 		dfechan.setBounds(145, 186, 138, 20);
@@ -152,5 +178,20 @@ public class pnlEmpleado extends JPanel {
 		dfechai = new JDateChooser();
 		dfechai.setBounds(399, 186, 138, 20);
 		add(dfechai);
+	}
+	public void limpiar()
+	{
+		txtidentidad.setText("");
+		txtnombre.setText("");
+		//cmbdepto.setSelectedIndex(0);
+		//dfechan.
+		//dfechai.
+		txtdireccion.setText("");
+		txtcorreoe.setText("");
+		//cmbestadoc.setSelectedIndex(0);
+		//cmbgenero.setSelectedIndex(0);
+		txtmovil.setText("");
+		txtotro.setText("");
+		//cmbestado.setSelectedIndex(0);
 	}
 }
